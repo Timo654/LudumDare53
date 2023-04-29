76c43cc39ac3a0b4dc43dd1578a65ef308100645
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-04-29 09:17:21 +0300

    platforming mechanics

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public enum GameState
+{
+    Start,
+    Running,
+    Delivery,
+    Win,
+    Lose,
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private GameObject MobileUI; // TODO
+    private GameState _currentState;
+    private int _currentHappiness = 10000;
+
+    private EventSystem EVRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            Debug.Log("mobile!");
+            MobileUI.SetActive(true);
+        }
+        EVRef = EventSystem.current; // get the current event system
+        OnGameStateChanged(GameState.Start);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnGameStateChanged(GameState newState)
+    {
+        if (_currentState.Equals(GameState.Win) || _currentState.Equals(GameState.Lose))
+        {
+            Debug.Log("Tried to change state after hitting an ending, ignoring.");
+            return;
+        }
+        _currentState = newState;
+        switch (_currentState)
+        {
+            case GameState.Start:
+                //TODO: Setup stuff for starting game
+                HandleStart();
+                break;
+            case GameState.Running:
+                //TODO: Handle running animation etc.
+                Debug.Log($"switched game state to running");
+                HandleRunning();
+                break;
+            case GameState.Delivery:
+                Debug.Log($"switched game state to running");
+                HandleDelivery();
+                break;
+            case GameState.Lose:
+                //TODO: Handle loss
+                HandleLoss();
+                Debug.Log($"switched game state to loss");
+                break;
+            case GameState.Win:
+                //TODO: Handle win
+                HandleWin();
+                Debug.Log($"switched game state to win");
+                break;
+            default:
+                Debug.Log("Unknown state passed to state manager");
+                break;
+        }
+    }
+
+    void HandleStart()
+    {
+        Debug.Log($"switched game state to start");
+        PlayerPrefs.SetInt("Score", 0);
+        player.position = startPos.position;
+        OnGameStateChanged(GameState.Running);
+    }
+
+    void HandleRunning()
+    {
+        UnpauseGame();
+    }
+
+    void HandleDelivery()
+    {
+        // TODO
+    }
+
+
+    void HandleLoss()
+    {
+        Time.timeScale = 1.0f;
+        StartCoroutine(DelaySceneLoad(2, "BadEnd"));
+    }
+
+    void HandleWin()
+    {
+        PlayerPrefs.SetInt("Happiness", _currentHappiness);
+        //SceneManager.LoadScene(5);
+        StartCoroutine(DelaySceneLoad(2, "GoodEnd"));
+    }
+
+    IEnumerator DelaySceneLoad(float delay, string scene)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        _currentHappiness += scoreToAdd;
+    }
+
+    public int GetHappiness()
+    {
+        return _currentHappiness;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
---
+++ Assets/Scripts/Player/Player_Walk.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player_Walk : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D body;
+    private PlayerControls playerControls;
+    private InputAction move;
+    private InputAction jump;
+    Player_GroundDetection ground;
+
+    [Header("Calculations")]
+    public float directionX;
+    private Vector2 desiredVelocity;
+    public Vector2 velocity;
+    private float maxSpeedChange;
+    private float acceleration;
+    private float deceleration;
+    private float turnSpeed;
+
+    [Header("Movement Stats")]
+    [SerializeField, Range(0f, 20f)] [Tooltip("Maximum movement speed")] public float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to reach max speed")] public float maxAcceleration = 52f;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop after letting go")] public float maxDecceleration = 52f;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop when changing direction")] public float maxTurnSpeed = 80f;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to reach max speed when in mid-air")] public float maxAirAcceleration;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop in mid-air when no direction is used")] public float maxAirDeceleration;
+    [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop when changing direction when in mid-air")] public float maxAirTurnSpeed = 80f;
+    [SerializeField] private Animator playerAnimator;
+    [Header("Current State")]
+    public bool onGround;
+    public bool pressingKey;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        ground = GetComponent<Player_GroundDetection>();
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+        move.started += OnMovement;
+        move.canceled += OnMovement;
+
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+    }
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        //This is called when you input a direction on a valid input type, such as arrow keys or analogue stick
+        //The value will read -1 when pressing left, 0 when idle, and 1 when pressing right.
+        if (!PauseMenu.GameIsPaused)
+        {
+            //Debug.Log("DirectionX: " + context.ReadValue<float>());
+            directionX = context.ReadValue<float>();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Used to flip the character's sprite when she changes direction
+        //Also tells us that we are currently pressing a direction button
+        if (directionX != 0)
+        {
+            transform.localScale = new Vector3(directionX > 0 ? 1 : -1, 1, 1);
+            pressingKey = true;
+        }
+        else
+        {
+            pressingKey = false;
+        }
+        playerAnimator.SetBool("Running", pressingKey);
+
+        desiredVelocity = new Vector2(directionX, 0f) * Mathf.Max(maxSpeed, 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        //Fixed update runs in sync with Unity's physics engine
+        onGround = ground.GetOnGround();
+        velocity = body.velocity;
+        runWithAcceleration();
+    }
+
+    private void runWithAcceleration()
+    {
+        //Set our acceleration, deceleration, and turn speed stats, based on whether we're on the ground on in the air
+
+        acceleration = onGround ? maxAcceleration : maxAirAcceleration;
+        deceleration = onGround ? maxDecceleration : maxAirDeceleration;
+        turnSpeed = onGround ? maxTurnSpeed : maxAirTurnSpeed;
+
+        if (pressingKey)
+        {                 
+            //If the sign (i.e. positive or negative) of our input direction doesn't match our movement, it means we're turning around and so should use the turn speed stat.
+            if (Mathf.Sign(directionX) != Mathf.Sign(velocity.x))
+            {
+                maxSpeedChange = turnSpeed * Time.deltaTime;
+            }
+            else
+            {
+                //If they match, it means we're simply running along and so should use the acceleration stat
+                maxSpeedChange = acceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            //And if we're not pressing a direction at all, use the deceleration stat
+            maxSpeedChange = deceleration * Time.deltaTime;
+        }
+        //Move our velocity towards the desired velocity, at the rate of the number calculated above
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        // TODO - add audio here
+        if (velocity.x != 0)
+        {
+            
+        }
+        else
+        {
+          
+        }
+        //Update the Rigidbody with this new velocity
+        body.velocity = velocity;
+    }
+
+}
