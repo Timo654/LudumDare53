561b23e73abcf6e2a320e4be0c4a1099e9a7c58f
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-09-29 16:53:47 +0300

    fading for opening

--- Assets/Scripts/OPScript.cs
+++ Assets/Scripts/OPScript.cs
@@ -1,32 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
 using FMODUnity;
+using DG.Tweening;
+using System.Collections;
 
 public class OPScript : MonoBehaviour
 {
     [SerializeField] Sprite[] openingSprites;
     [SerializeField] EventReference[] sfxRef;
     int currentSprite = 0;
-    bool isFinished = false;
     public string nextScene = "LV1_Delivery";
+    bool inputBlocked = false;
+    Image _nextImage;
     Image _image;
     // Start is called before the first frame update
     void Start()
     {
-        _image = GetComponent<Image>();
+        _image = transform.GetChild(1).GetComponent<Image>();
         _image.sprite = openingSprites[currentSprite];
+        _nextImage = transform.GetChild(2).GetComponent<Image>();
         //AudioManager._instance.InitializeMusic(FMODEvents.instance.mainmusic);
         //RuntimeManager.PlayOneShot(sfxRef[currentSprite]); // do we really want to play a sound right away?
     }
     void Update()
     {
-        if (Input.anyKeyDown & !isFinished)
+        if (Input.anyKeyDown & !inputBlocked)
         {
             currentSprite += 1;
             if (currentSprite < openingSprites.Length)
             {
-                _image.sprite = openingSprites[currentSprite];
-                //RuntimeManager.PlayOneShot(sfxRef[currentSprite]);
+                StartCoroutine(FadeSprites(openingSprites[currentSprite]));
+                //RuntimeManager.PlayOneShot(sfxRef[currentSprite]); // could play in coroutine so it plays after it fully switches? dunno
             }
             else
             {
@@ -37,5 +41,21 @@ public class OPScript : MonoBehaviour
     public void OnOpeningEnd()
     {
         LevelChangerScript._instance.FadeToLevel(nextScene);
+    }
+
+    IEnumerator FadeSprites(Sprite newSprite)
+    {
+        _nextImage.sprite = newSprite;
+        _image.DOFade(0.0f, 1.5f);
+        _nextImage.DOFade(1.0f, 1f);
+        inputBlocked = true;
+        yield return new WaitForSecondsRealtime(1f);
+        _nextImage.DOKill(); // just in case
+        _image.DOKill();
+        _image.sprite = newSprite;
+        _image.DOFade(1.0f, 0f);
+        _nextImage.DOFade(0.0f, 0f);
+        inputBlocked = false;
+
     }
 }
