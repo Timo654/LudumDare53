f35b1f00e8c1f0bd674312ddf2056c3f50a491d7
Timo654 <41972888+Timo654@users.noreply.github.com>
2023-10-06 16:47:42 +0300

    improve cutscene transitions

--- Assets/Scripts/OPScript.cs
+++ Assets/Scripts/OPScript.cs
@@ -38,9 +38,9 @@ public class OPScript : MonoBehaviour
 
     void Start()
     {
-        _image = transform.GetChild(1).GetComponent<Image>();
+        _image = transform.GetChild(2).GetComponent<Image>();
         _image.sprite = openingSprites[currentSprite];
-        _nextImage = transform.GetChild(2).GetComponent<Image>();
+        _nextImage = transform.GetChild(1).GetComponent<Image>();
         AudioManager._instance.InitializeMusic(music);
         if (currentSprite < sfxRef.Length)
             RuntimeManager.PlayOneShot(sfxRef[currentSprite]); 
@@ -75,15 +75,12 @@ public class OPScript : MonoBehaviour
     IEnumerator FadeSprites(Sprite newSprite)
     {
         _nextImage.sprite = newSprite;
-        _image.DOFade(0.0f, 1.5f);
-        _nextImage.DOFade(1.0f, 1f);
+        _image.DOFade(0.0f, 1f);
         inputBlocked = true;
         yield return new WaitForSecondsRealtime(1f);
-        _nextImage.DOKill(); // just in case
         _image.DOKill();
         _image.sprite = newSprite;
         _image.DOFade(1.0f, 0f);
-        _nextImage.DOFade(0.0f, 0f);
         inputBlocked = false;
 
     }
